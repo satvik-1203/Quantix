1866ad9a9b8c57d2df28f8b111a1ad50b49adf78
William <wc2182@nyu.edu>
2025-10-06 10:25:21 -0400

    updated connections

--- apps/server/src/routers/call-subtest/controller.ts
+++ apps/server/src/routers/call-subtest/controller.ts
@@ -19,16 +19,22 @@ router.post("/", async (req, res) => {
     throw new Error("Sub test not found");
   }
 
+  if (!subTest[0].test_cases.testPhoneNumber) {
+    throw new Error("Test phone number not found");
+  }
+
   const [subTestData] = subTest;
 
   const callingAgentDescription = subTestData.test_cases.description;
 
   const taskDescription = subTestData.sub_tests.description;
 
+  // const taskExpected = subTestData.sub_tests.expected;
+
   const result = await vapi.calls.create({
     assistantId: "e6d0707e-8347-4c09-a93f-af1eed22fffe",
     customer: {
-      number: "+19494321144",
+      number: subTestData.test_cases.testPhoneNumber as string,
     },
     phoneNumberId: "61077b28-602c-4417-9155-3b0ef6cb2d88",
     assistantOverrides: {
